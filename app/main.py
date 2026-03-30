f19bd99d277392691bff1c4282e75edcaae89092
muhammadanassheikh <anassheikh996@gmail.com>
2026-03-30 15:16:03 +0500

    final clean commit

--- app/main.py
+++ app/main.py
@@ -4,6 +4,11 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
+<<<<<<< HEAD
+=======
+    print('this is line one')
+    print('this is line two')
+>>>>>>> 1bd2086 (final clean commit)
     print('adding log')
     return {"message": "Hello, FastAPI! updated responnse... adding something"}
 
